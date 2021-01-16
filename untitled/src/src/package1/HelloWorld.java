6510f80e62158180e2792df6153f5f1041b3d6c7
一鸣 李 <1361154598@qq.com>
2021-01-16 11:27:00 +0800

    Default Changelist

---
+++ untitled/src/src/package1/HelloWorld.java
@@ -0,0 +1,7 @@
+package package1;
+
+/**
+ * Created by KB on 2021/1/11.
+ */
+public class HelloWorld {
+}
---
+++ untitled/src/src/package1/aaa.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class aaa {
+    public static  void main(String[] args){
+        System.out.println("Im back!");
+    }
+}
